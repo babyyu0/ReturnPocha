852cefc010e294049f0a9dbee0184f8406dc478d
You Young <dbdud1999@naver.com>
2023-08-24 17:53:46 +0900

    :wranch: axios 구성 파일 수정

--- frontend/src/service/axios.js
+++ frontend/src/service/axios.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const api = axios.create({
     baseURL: "http://localhost:8080",
     header: {
-      Accept: "application/json",
       "Content-type": "application/json;charset=UTF-8",
     },
     method: "POST"
