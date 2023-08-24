279f5941a6aab9c6733b38fe08cd7ab01804cebc
You Young <dbdud1999@naver.com>
2023-08-24 17:51:56 +0900

    :sparkles: Room Page 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,20 @@
 import EnterPage from './enter/EnterPage';
+import RoomPage from './room/RoomPage';
 import './resource/css/globals.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
 function App() {
   return (
+    <RecoilRoot>
       <BrowserRouter>
         <div id="header"></div>
         <Routes>
-          <Route path='/' element={ <EnterPage /> } />
+          <Route path='/' element={<EnterPage />} />
+          <Route path='/room' element={<RoomPage />} />
         </Routes>
       </BrowserRouter>
+    </RecoilRoot>
   );
 }
 
---
+++ frontend/src/room/RoomPage.js
@@ -0,0 +1,8 @@
+function RoomPage() {
+
+    return (
+        <div></div>
+    );
+}
+
+export default RoomPage;
