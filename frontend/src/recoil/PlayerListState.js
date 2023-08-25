b70564ac0b3f8e4df363f5db088bd84685e90d5d
You Young <dbdud1999@naver.com>
2023-08-25 14:50:56 +0900

    :art: Player 및 Player List 저장 구조 변경

--- frontend/src/recoil/PlayerListState.js
+++ frontend/src/recoil/PlayerListState.js
@@ -1,5 +1,10 @@
 import { atom } from "recoil";
 
+export const PlayerState = atom({
+    key: 'PlayerState',
+    default: ''
+});
+
 export const PlayerListState = atom({
     key: 'PlayerListState',
     default: {}
