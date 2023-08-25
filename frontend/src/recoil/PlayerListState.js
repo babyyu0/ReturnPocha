1e18af68ca527eb6fa2f6bad3f1b8ca94f7937f8
You Young <dbdud1999@naver.com>
2023-08-25 16:17:09 +0900

    :sparkles: 세션 스토리지 추가

--- frontend/src/recoil/PlayerListState.js
+++ frontend/src/recoil/PlayerListState.js
@@ -1,11 +1,19 @@
+import { recoilPersist } from 'recoil-persist';
 import { atom } from "recoil";
+
+const { persistAtom } = recoilPersist({
+    key: 'sessionStorage',
+    storage: sessionStorage,
+  });
 
 export const PlayerState = atom({
     key: 'PlayerState',
-    default: ''
+    default: '',
+    effects_UNSTABLE: [persistAtom]
 });
 
 export const PlayerListState = atom({
     key: 'PlayerListState',
-    default: {}
+    default: {},
+    effects_UNSTABLE: [persistAtom]
 });
--- frontend/src/recoil/RoomState.js
+++ frontend/src/recoil/RoomState.js
@@ -1,9 +1,16 @@
+import { recoilPersist } from 'recoil-persist';
 import { atom } from "recoil";
+
+const { persistAtom } = recoilPersist({
+    key: 'sessionStorage',
+    storage: sessionStorage,
+  });
 
 export const RoomState = atom({
     key: 'RoomState',
     default: {
         id: '',
         progress: false
-    }
+    },
+    effects_UNSTABLE: [persistAtom]
 });
