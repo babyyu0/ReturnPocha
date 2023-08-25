4597bad95b85e9a52b25ca3326f3f320c27e1e43
유영 <dbdud1999@naver.com>
2023-08-26 01:24:54 +0900

    :art: useEffect 두 번 실행 방지를 위한 Strict Mode 해제

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,20 @@
-import EnterContainer from './container/EnterContainer';
-import RoomContainer from './container/RoomContainer';
-import './resource/css/globals.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
+import EnterContainer from "./container/EnterContainer";
+import RoomContainer from "./container/RoomContainer";
+import "./resource/css/globals.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 
 function App() {
   return (
     <RecoilRoot>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<EnterContainer />} />
-          <Route path='/:roomId' element={<EnterContainer />} />
-          <Route path='/room' element={<RoomContainer />} />
+          <Route path="/" element={<EnterContainer />} />
+          <Route path="/:roomId" element={<EnterContainer />} />
+          <Route path="/room" element={<RoomContainer />} />
         </Routes>
       </BrowserRouter>
     </RecoilRoot>
-
   );
 }
 
